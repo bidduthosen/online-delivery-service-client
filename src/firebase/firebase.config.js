18fd87c7abb17a3cc28df23bb3b404fec6e4ee35
bidduthosen <mdbidduthosen34@gmail.com>
2022-11-07 23:21:12 +0600

    create react router app and install all doc and add the website title

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvZIlqrULZ5bAwToweISPOe5mgPsdZI4U",
+  authDomain: "online-delivery-service-e157f.firebaseapp.com",
+  projectId: "online-delivery-service-e157f",
+  storageBucket: "online-delivery-service-e157f.appspot.com",
+  messagingSenderId: "1076564423327",
+  appId: "1:1076564423327:web:0941caf2465986250828aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
