d4de911d564a2f8d39e3965e42687bb66a9e9b35
bidduthosen <mdbidduthosen34@gmail.com>
2022-11-09 17:09:42 +0600

    review form done and conditional randaring done

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,27 +1,100 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaCrown, FaDollarSign, FaStar, FaStarHalfAlt } from 'react-icons/fa';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const ServiceDetails = () => {
+    const {user} = useContext(AuthContext);
     const serviceDetails = useLoaderData();
-    const  {title, price, img, description, rating} = serviceDetails;
+    const  {service_id, title, price, img, description, rating} = serviceDetails;
+
+
+    const handleInReview = event =>{
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const message = form.message.value;
+        
+        const review = {
+            service_id: service_id,
+            name,
+            title: title,
+            img:img,
+            rating: rating,
+            email: user?.email,
+            photoURL,
+            message,
+            
+        }
+        console.log(review)
+
+    }
     return (
-        <div className='m-3'>
-            <div className="card w-full bg-base-100 shadow-xl border-2 border-success-500 my-7">
-            <div className="font-bold text-3xl text-dark text-center py-3 mb-12 w-1/5 mx-auto border-b-8 border-secondary-900">Product information details</div>
-            <figure><img src={img} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                {title}
-                <sub><FaCrown className='text-secondary'></FaCrown> </sub>
-                </h2>
-                <p className='italic'>{description}</p>
-                <div className="card-actions justify-end mt-3">
-                <div className="badge badge-outline p-3"><FaStar/> <FaStar/> <FaStarHalfAlt className='mr-1'/> {rating}</div>
-                <div className="badge badge-outline p-3">Price: <FaDollarSign></FaDollarSign>{price}</div>
+        <div>
+            <div className='m-3'>
+                <div className="card w-full bg-base-100 shadow-xl border-2 border-success-500 my-7">
+                    <div className="font-bold text-3xl text-dark text-center py-3 mb-12 w-1/5 mx-auto border-b-8 border-secondary-900">Product information details</div>
+                    <figure><img src={img} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                        {title}
+                        <sub><FaCrown className='text-secondary'></FaCrown> </sub>
+                        </h2>
+                        <p className='italic'>{description}</p>
+                        <div className="card-actions justify-end mt-3">
+                        <div className="badge badge-outline p-3"><FaStar/> <FaStar/> <FaStarHalfAlt className='mr-1'/> {rating}</div>
+                        <div className="badge badge-outline p-3">Price: <FaDollarSign></FaDollarSign>{price}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div>
+                {user ? 
+                <>
+                    <div className="hero py-10  bg-base-200">
+                        <div className="hero-content w-4/5">
+                            <div className="card flex-shrink-0 w-full shadow-2xl" style={{background:"#ffffffb5"}}>
+                            <div className="font-bold text-2xl text-dark text-center py-3 md:w-1/5 mx-auto my-6  border-4 border-double border-sky-400  p-4 hover:border-solid">Check Out Form</div>
+                            <form onSubmit={handleInReview}  className="card-body">
+                                <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" required/>
+                                </div>
+                                <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' defaultValue={user?.email} readOnly placeholder="email" className="input input-bordered" required/>
+                                </div>
+                                <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text" name='photoURL' placeholder="PhotoURL" className="input input-bordered" required/>
+                                </div>
+                                <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Message</span>
+                                </label>
+                                <textarea name='message' className="textarea textarea-bordered h-28" placeholder="Message" required></textarea>
+                                </div>
+                                <div className="form-control mt-6">
+                                <button className="btn btn-primary md:w-1/5">Check Out</button>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                </>
+                :
+                <>
+                    <p className='my-6 text-2xl w-2/5 mx-auto text-center border-4 border-double border-sky-400  p-4 hover:border-solid'>Please? <Link className='text-orange-600' to='/login'>Login</Link> to add a review</p>
+                </>
+                }
+            </div>
         </div>
     );
 };
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvZIlqrULZ5bAwToweISPOe5mgPsdZI4U",
-  authDomain: "online-delivery-service-e157f.firebaseapp.com",
-  projectId: "online-delivery-service-e157f",
-  storageBucket: "online-delivery-service-e157f.appspot.com",
-  messagingSenderId: "1076564423327",
-  appId: "1:1076564423327:web:0941caf2465986250828aa"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
